94b2ecaf5c8ae3cbf116ff319d122874eea277e2
josh cooter <thejoshcooter@gmail.com>
2019-05-03 11:15:27 -0500

    add prop-type and tests

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = ({selectTabHandler, selectedTab, tab}) => {
   return (
@@ -12,5 +13,11 @@ const Tab = ({selectTabHandler, selectedTab, tab}) => {
 };
 
 // Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  tab: PropTypes.string.isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired
+}
 
 export default Tab;
